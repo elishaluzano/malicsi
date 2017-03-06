42779f2fb0a25d520437f66e477489cc6d204cb7
Samuel Bautista <tadeolinco@gmail.com>
2017-03-06 23:08:18 +0800

    Setup

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # malicsi
-CMSC 128 2nd Semester AY 2016-2017
+### CMSC 128 2nd Semester AY 2016-2017
+***
+### How to install
+Run `npm install`. If semantic-ui doesn't install, run `sudo npm install -g gulp` and try installing again.
+
+***
+### How to run
+1. On one terminal, run `npm run build`
+2. On another terminal, run `npm start`
+
+***
+### Project Directory
+```javascript
+dist/
+node_modules/
+src/
+---- client/
+-------- assets/
+-------- other_folders/
+-------- app.js
+-------- index.html
+---- server/
+-------- controllers
+-------- test/
+-------- server.js
+---- vendor.js
+```
+* **dist/** - will contain all static files that will be produced by webpack
+* **node_modules/** - contains all modules installed through npm
+* **src/** - all source files, we code here
+    * **client** - front-end code, angularJS
+        * **assets/** - other static files eg. images, sound, etc
+        * **index.html** - single page app
+    * **server/** - back-end code
+        * **controllers/** - functions to be defined per data entity
+        * **routes/** - mapping of functions to API endpoints
+        * **test/** - test code
+        * **server.js** - server entry point file
+    * **vendor.js** - all 3rd party libraries needed by front-end will be required here
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = function(env) {
+    return {
+        entry: {
+            main: './src/client/app.js',
+            vendor: './src/vendor.js'
+        },
+        output: {
+            filename: '[chunkhash].[name].js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: ExtractTextPlugin.extract({
+                        use: 'css-loader'
+                    })
+                },
+                {
+                    test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                    use: "url-loader?limit=10000&mimetype=application/font-woff"
+                },
+                { 
+                    test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+                    use: "file-loader" 
+                }, 
+                {
+                    test: /\.(jpg|gif|png)$/,
+                    use: 'file-loader?name=images/[name].[ext]'
+                }
+            ]
+        },
+        plugins: [
+            new webpack.optimize.CommonsChunkPlugin({
+                name: ['vendor', 'manifest']
+            }),
+            new ExtractTextPlugin('style.css'),
+            new HtmlWebpackPlugin({
+                template: './src/app/index.html'
+            })
+        ]
+    }
+}
