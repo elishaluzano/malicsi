a69f77f3895ab1b1b046206745e3f90617c5e06b
mazefernandez <aafernandez5@up.edu.ph>
2017-03-08 15:27:16 +0800

    added routes to server

---
+++ src/server/routes/routes.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var express = require('express');
+var router = express.Router();
+
+//insert routes here
+/*
+EXAMPLE 
+var sampleCtrl = require('../controllers/sampleController.js')
+router.post('/getSomething', sampleCtrl.getSomething);
+router.post('/insertSomething', sampleCtrl.insertSomething);
+*/
+
+module.exports = router;
+
