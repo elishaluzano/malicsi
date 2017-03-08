b5eda3004885de0b9edaf1a057e528dc449b8de9
mazefernandez <aafernandez5@up.edu.ph>
2017-03-08 15:29:58 +0800

    modified the setup of server.js

---
+++ src/server/controllers/sampleController.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const connection = require('./../database.js')
+
+
+/*
+EXAMPLE 
+
+exports.getSomething = (req,res) => {
+	connection.query('SELECT * FROM ___ WHERE = ?', [req.body.___], function(err, rows, fields){
+		res.send(rows);
+	});
+}
+exports.postSomething = (req,res) => {
+	connection.query('INSERT INTO __(_,_) VALUES(_,_) ', [req.body.__,req.body.__], function(err, rows, fields){
+		res.send(rows);
+	});
+*/
+}
