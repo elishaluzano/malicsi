446386fe74afddea793729bb71e26733a8c8557d
mazefernandez <aafernandez5@up.edu.ph>
2017-03-15 14:34:45 +0800

    modified admin and user controller

--- src/server/controllers/adminController.js
+++ src/server/controllers/adminController.js
@@ -1,10 +1,10 @@
 'use strict'
 
 const mysql = require('mysql');
-const connection = require('./../database.js')
+const connection = require(__dirname + '/../database.js');
 
 // GET all admins
-exports.getAllAdmins = (req, res) => {
+exports.getAdmins = (req, res) => {
 	connection.query('SELECT * FROM admin',[], function(err, rows, fields) {
 		if (!err) {
 			res.send(rows);	
@@ -15,6 +15,56 @@ exports.getAllAdmins = (req, res) => {
 		}
 	});
 }
+
+// GET specific admin
+exports.getAdmin = (req, res) => {
+	connection.query('SELECT FROM admin where admin_id ?',[req.params.id], function(err, rows, fields) {
+		if (!err) {
+			res.send(rows[0]);
+			console.log("Successfully retrieved an admin");
+		}
+		else {
+			console.log("Error in retrieving an admin");
+		}
+	});
+}
+
+// POST admin
+exports.addAdmin = (req, res) => {
+	connection.query('INSERT INTO admin SET ?',[req.body.admin_id, req.body.position], function(err, rows, fields) {
+		if (!err) {
+			console.log("Successfully added an admin");
+		}
+		else {
+			console.log("Error in adding an admin");
+		}
+	});
+}
+
+// PUT specific admin 
+exports.updateAdmin = (req, res) => {
+	connection.query('UPDATE admin SET position = ? where admin_id = ?',[req.body.position, req.body.admin_id], function(err, rows, fields) {
+		if (!err) {
+			console.log("Successfully updated an admin");
+		}
+		else {
+			console.log("Error in updating an admin");
+		}
+	});
+}
+
+// DELETE specific admin
+exports.deleteAdmin = (req, res) => {
+	connection.query('DELETE FROM admin where admin_id = ?',[req.params.id],function(err, rows, fields) {
+		if (!err) {
+			console.log("Successfully deleted an admin");
+		}
+		else {
+			console.log("Error in deleting an admin");
+		}
+	});
+}
+
 /*
 // GET all admins under a sponsoring institution 
 exports.getAdmins = (req, res) => {
--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -4,19 +4,24 @@ var express = require('express');
 var router = express.Router();
 
 //insert routes here
-
 var userCtrl = require('../controllers/userController.js');
 //User Account routes
-router.post('/apiGetAccount', userCtrl.getAccount);
-router.post('/apiLogin', userCtrl.loginUser);
-router.post('/apiSignup', userCtrl.addUser);
+router.post('/api/login', userCtrl.loginUser);
+router.post('/api/users/getAccount', userCtrl.getAccount);
+router.post('/api/users/signup', userCtrl.addUser);
 //User routes
-router.post('/apiAddUser', userCtrl.addUser);
-router.post('/apiGetUsers', userCtrl.getUsers);
-router.post('/apiGetUser', userCtrl.getUser);
-//router.put('/updateUser', userCtrl.updateUser);
-router.delete('/apiDeleteUser', userCtrl.deleteUser);
+router.get('/api/users/getUsers', userCtrl.getUsers);
+router.get('/api/users/:id', userCtrl.getUser);
+router.put('/api/users/updateUser', userCtrl.updateUser);
+router.delete('/api/users/:id', userCtrl.deleteUser);
 
+//Admin routes
+var adminCtrl = require('../controllers/adminController.js');
+router.get('/api/admins/getAdmins', adminCtrl.getAdmins);
+router.get('/api/admins/getAdmin', adminCtrl.getAdmin);
+router.post('/api/admins/addAdmin', adminCtrl.addAdmin);
+router.put('/api/admins/updateAdmin', adminCtrl.updateAdmin);
+router.delete('api/admins/:id', adminCtrl.deleteAdmin);
 
 module.exports = router;
 
