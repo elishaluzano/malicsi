30da6fcdd65522d4bd8dc4332203d32ab3ac085f
andiesantos <amsantos12@up.edu.ph>
2017-03-18 19:09:19 +0800

    Made changes in JS of edtProfile, changed script directories

---
+++ src/templates/profile_page/editProfile.js
@@ -0,0 +1,47 @@
+$(document).ready(()=>{
+    /* 
+    Until there is a solution to the pickadate bug, we're using different inputs.
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
+    $('.datepicker').on('close', ()=>{
+        $(document.activeElement).blur();
+    });
+    */
+    var numAffRow = 1;
+    
+    //===PLEASE CONFIRM===
+    //$('select').material_select();
+    //$('.modal').modal();
+    
+    expandRow = (item) => {
+        item.on("click", () => {
+            console.log("aneq");
+            $("#affiliations .affiliation-row:last-child").clone().appendTo($("#affiliations"));
+            expandRow($("#affiliations .affiliation-row:last-child .add-affiliation"));
+            deleteRow($("#affiliations .affiliation-row:last-child .remove-affiliation"))
+            numAffRow = numAffRow + 1;
+            checkAffRows();
+        });
+    }
+
+    deleteRow = (item) => {
+        item.on("click", () => {
+            item.parent().parent().remove();
+            numAffRow = numAffRow - 1;
+            checkAffRows();
+        });
+    }
+
+    checkAffRows = () => {
+        if (numAffRow >= 2) {
+            $(".remove-affiliation").removeClass("disabled");
+        } else {
+            $(".remove-affiliation").addClass("disabled");
+        }
+    }
+
+    expandRow($(".add-affiliation"));
+    deleteRow($(".remove-affiliation"));
+});
