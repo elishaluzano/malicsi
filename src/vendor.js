170f9d8d919a666e6c1439f27f377efbed03e182
Samuel Bautista <tadeolinco@gmail.com>
2017-03-20 11:18:56 +0800

    Add webui-popover to vendors

--- src/vendor.js
+++ src/vendor.js
@@ -3,3 +3,5 @@ require('angular-ui-router');
 require('jquery');
 require('materialize-css/dist/css/materialize.css');
 require('materialize-css/dist/js/materialize.js');
+require('webui-popover/dist/jquery.webui-popover.css');
+require('webui-popover/dist/jquery.webui-popover.js');
