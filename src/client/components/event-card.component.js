749d3c4bf615a21f4dce2a489f386d9b5f26c8f9
NickMemebulan <bisimbulan@up.edu.ph>
2017-03-21 23:30:38 +0800

    venue.service

---
+++ src/client/components/event-card.component.js
@@ -0,0 +1,19 @@
+
+(function () {
+	'use strict'
+
+	angular
+		.module('app');
+		.component('eventCard', {
+			template: require('./event-card.html'),
+			controller: eventCardController,
+			bindings: {
+				event: '<'
+			}
+		});
+
+	function eventCardController(){
+		var vm = this;
+	}
+
+})();
