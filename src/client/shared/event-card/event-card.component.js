5b398b2a1f2a44d6f7abe3133a70012982a023b4
NickMemebulan <bisimbulan@up.edu.ph>
2017-03-24 21:33:36 +0800

    added event-card component and html

--- src/client/shared/event-card/event-card.component.js
+++ src/client/shared/event-card/event-card.component.js
@@ -1,5 +1,5 @@
 (function () {
-    'use strict'
+    'use strict';
 
     angular
         .module('app')
@@ -7,12 +7,44 @@
             template: require('./event-card.html'),
             controller: eventCardController,
             bindings: {
-                event: '<'
+                event: '<',
+                isAdmin: '<'
             }
         });
 
-    function eventCardController(){
+    function eventCardController(institutionService) {
         var vm = this;
+
+        vm.eventStatus = {
+            color: '',
+            text: ''
+        };
+        
+        var newDate = null;
+
+        vm.$onInit = function() {
+            console.log(vm.event);
+            institutionService.getOne(event.institution_id_key)
+                .then(function(data) {
+                    if (data) {
+                        vm.institutionName = data.name;
+                    }
+                });
+
+            newDate = new Date().getTime();
+            if (newDate > new Date(event.start_date).getTime && newDate < new Date(event.end_date).getTime) {
+                vm.eventStatus.color = 'green';
+                vm.eventStatus.text = 'Live';
+            }
+            else if (newDate < new Date(event.start_date).getTime) {
+                vm.eventStatus.color = 'red';
+                vm.eventStatus.text = 'Soon';
+            }
+            else {
+                vm.eventStatus.color = 'black';
+                vm.eventStatus.text = 'Done';
+            }
+        }
     }
 
 })();
