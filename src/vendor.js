6d3989dad2e58c4a27482562372083388c905ca1
Sam Bautista <tadeolinco@gmail.com>
2017-03-24 18:15:36 +0800

    Added material icons; Added global css

--- src/vendor.js
+++ src/vendor.js
@@ -3,5 +3,7 @@ require('angular-ui-router');
 require('jquery');
 require('materialize-css/dist/css/materialize.css');
 require('materialize-css/dist/js/materialize.js');
+require('material-design-icons/iconfont/material-icons.css');
 require('webui-popover/dist/jquery.webui-popover.css');
 require('webui-popover/dist/jquery.webui-popover.js');
+require('./client/style.css');
