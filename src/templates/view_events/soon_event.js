bbea09b6c5ad52e4439ab7875ebe420de5f425bd
andiesantos <amsantos12@up.edu.ph>
2017-03-30 01:52:43 +0800

    Edited modal for adding a game schedule

--- src/templates/view_events/soon_event.js
+++ src/templates/view_events/soon_event.js
@@ -2,8 +2,8 @@ var myCountdown1 = new Countdown({
 	year  	: 2017,  // (optional) The target date's year
 	month 	: 11,     // (optional) The target date's month
 	day   	: 19,     // (optional) The target date's day
-	width:450, 
-	height:100,  
+	//width:450, 
+	//height:100,  
 	rangeHi:"month",
 	style:"flip"	// <- no comma on last item!
 });
