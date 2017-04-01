3810ff8da3bfd1cc0ddc1cd08cd01796bb1e8e2a
djbelmonte <belmontedonnjerico@gmail.com>
2017-04-01 10:04:20 +0800

    Added datepicker js -belmonte

--- src/templates/view_events/view_events.js
+++ src/templates/view_events/view_events.js
@@ -20,3 +20,9 @@ function autoplay() {
 $('#login').webuiPopover({url:'#login-form'});
 
 $('#drop-links').webuiPopover({url:'#additional-links'});
+
+ $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year
+    container: 'body'
+  });
