40f166d0c32b575a6109dd7eb152b1a7759cd4f2
Samuel Bautista <tadeolinco@gmail.com>
2017-04-01 20:16:38 +0800

    Fixed footer

--- src/client/shared/footer-card/footer-card.component.js
+++ src/client/shared/footer-card/footer-card.component.js
@@ -4,8 +4,16 @@
         .module('app')
         .component('footerCard',{
             template: require('./footer-card.html'),
-            
+            controller: footerCardController 
         });
 
+    function footerCardController($state) {
+        var vm = this;
+
+        vm.toTop = function() {
+            console.log('scrolling');
+            $('html, body').animate({ scrollTop: 0 }, 500);
+        }
+    }
   
 })();
