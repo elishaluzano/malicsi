4b5530d3d8379c6cb63a746938c1b9ab7d45e22f
Earvin Leonardo <ivleonardo@up.edu.ph>
2017-04-05 03:22:46 +0800

    added slider

---
+++ src/templates/index_page/index.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    Materialize.updateTextFields();
+});
+
+$('.side-nav-button').sideNav({
+    menuWidth: 300,
+    edge: 'right',
+    closeOnClick: true
+});
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
+
+$('.carousel.carousel-slider').carousel({fullWidth: true});
+autoplay()   
+function autoplay() {
+    $('.carousel.carousel-slider').carousel('next');
+    setTimeout(autoplay, 6000);
+}        
+
+$('#login').webuiPopover({url:'#login-form'});
+
+$('#drop-links').webuiPopover({url:'#additional-links'});
+
+  $(document).ready(function(){
+      $('.slider').slider();
+    });
