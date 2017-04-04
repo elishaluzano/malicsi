4df21aaac1ca2cdf4f106fa25a9adc6b7bc1095d
Samuel Bautista <tadeolinco@gmail.com>
2017-04-04 12:55:34 +0800

    Fix user update validation

--- src/client/components/user-page/user-page.component.js
+++ src/client/components/user-page/user-page.component.js
@@ -27,26 +27,69 @@
 
         vm.toggleEdit = function() {
             if (vm.isBeingEdited) {
-                check();
-                var fd = new FormData();
-                fd.append('name', vm.user.name);
-                fd.append('username', vm.user.username);
-                fd.append('password', vm.user.password);
-                fd.append('email', vm.user.email);
-                fd.append('contact_number', vm.user.contact_number);
-                fd.append('birthday', (new Date(vm.user.birthday)).toISOString().substring(0,10));
-                fd.append('isOverallAdmin', vm.user.isOverallAdmin);
-                fd.append('contact_person', vm.user.contact_person);
-                fd.append('gender', vm.user.gender);
-                fd.append('profile_pic', (vm.files[0])? vm.files[0] : vm.user.profile_pic);
-                console.log(fd);
-                userService.update(vm.user.user_id, fd)
-                    .then(function(user) {
-                        vm.user = user;
-                        vm.user.birthday = new Date(vm.user.birthday);
-                        console.log(vm.user);
-                        vm.isBeingEdited = false;
+                if (!vm.user.name) {
+                    return Materialize.toast("What's your name?", 3000, 'red');
+                }
+
+                if (!vm.user.birthday) {
+                    return Materialize.toast('When is your birthday?', 3000, 'red');
+                }
+
+                if (!vm.user.gender) {
+                    return Materialize.toast('Are you male of female', 3000, 'red');
+                }
+
+                if (!vm.user.contact_number.length) {
+                    return Materialize.toast('Can I get your digits?', 3000, 'red');
+                }
+
+                if (isNaN(vm.user.contact_number)) {
+                    return Materialize.toast('Is that really your contact number?', 3000, 'red');
+                }
+
+                if (/[\W]/.exec(vm.user.username)) {
+                    return Materialize.toast('Username should only consist of alphanumeric characters and underscores', 3000, 'red');
+                }
+
+                if (vm.user.password.length < 8) {
+                    return Materialize.toast('Your password should be at least 8 characters long', 3000, 'red');
+                }
+
+                if (!/^\w+@[a-zA-Z]+\.[a-zA-Z]+/.exec(vm.user.email)) {
+                    return Materialize.toast('Is that a real email address?', 3000, 'red');
+                }
+
+                searchService.users(vm.user.username)
+                    .then(function(users) {
+                        let found = false;
+                        for (let user of users) {
+                            if (user.username === vm.user.username && user.user_id != vm.user.user_id) {
+                                found = true;
+                                Materialize.toast('Username has already been taken', 3000, 'red');
+                                break;
+                            }
+                        }
+                        if (!found) {
+                            var fd = new FormData();
+                            fd.append('name', vm.user.name);
+                            fd.append('username', vm.user.username);
+                            fd.append('password', vm.user.password);
+                            fd.append('email', vm.user.email);
+                            fd.append('contact_number', vm.user.contact_number);
+                            fd.append('birthday', (new Date(vm.user.birthday)).toISOString().substring(0,10));
+                            fd.append('isOverallAdmin', vm.user.isOverallAdmin);
+                            fd.append('contact_person', vm.user.contact_person);
+                            fd.append('gender', vm.user.gender);
+                            fd.append('profile_pic', (vm.files[0])? vm.files[0] : vm.user.profile_pic);
+                            userService.update(vm.user.user_id, fd)
+                                .then(function(user) {
+                                    vm.user = user;
+                                    vm.user.birthday = new Date(vm.user.birthday);
+                                    vm.isBeingEdited = false;
+                                });
+                        }
                     });
+
             }
             else {
                 vm.isBeingEdited = true;
@@ -59,56 +102,6 @@
                     Materialize.toast('You account has been deleted', 3000, 'red');
                     $state.go('landingPage');
                 });
-        }
-
-        function check() {
-            if (!vm.user.name) {
-                return Materialize.toast("What's your name?", 3000, 'red');
-            }
-
-            if (!vm.user.birthday) {
-                return Materialize.toast('When is your birthday?', 3000, 'red');
-            }
-
-            if (!vm.user.gender) {
-                return Materialize.toast('Are you male of female', 3000, 'red');
-            }
-
-            if (!vm.user.contact_number.length) {
-                return Materialize.toast('Can I get your digits?', 3000, 'red');
-            }
-
-            if (isNaN(vm.user.contact_number)) {
-                return Materialize.toast('Is that really your contact number?', 3000, 'red');
-            }
-
-            if (/[\W]/.exec(vm.user.username)) {
-                return Materialize.toast('Username should only consist of alphanumeric characters and underscores', 3000, 'red');
-            }
-
-            if (vm.user.password.length < 8) {
-                return Materialize.toast('Your password should be at least 8 characters long', 3000, 'red');
-            }
-
-            if (!/^\w+@[a-zA-Z]+\.[a-zA-Z]+/.exec(vm.user.email)) {
-                return Materialize.toast('Is that a real email address?', 3000, 'red');
-            }
-
-            searchService.users(vm.user.username)
-                .then(function(users) {
-                    let found = false;
-                    for (let user of users) {
-                        if (user.username === vm.user.username) {
-                            found = true;
-                            Materialize.toast('Username has already been taken', 3000, 'red');
-                            break;
-                        }
-                    }
-                    if (!found) {
-                        
-                    }
-                });
-
         }
         
     };
