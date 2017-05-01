cf5cf5e5cbd7371c0cd5b0b0ff1ea83ac1a61f70
Samuel Bautista <tadeolinco@gmail.com>
2017-05-01 14:08:56 +0800

    Fix timezone

--- src/client/services/user-log.service.js
+++ src/client/services/user-log.service.js
@@ -32,8 +32,7 @@
             },
 
             create: function(body) {
-                console.log(new Date().toISOString());
-                console.log(body);
+                body.activity_time = new Date(new Date().getTime() + 60*60*1000*8).toISOString().replace('T', ' ').substring(0, 19);
                 return $http.post(api, body) // tentative
                     .then(function(response) {
                         return response.data;
