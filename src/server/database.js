c0321730060aeff902ff1730605d59c3ac6b112c
kathleenarriola <ksarriola@up.edu.ph>
2017-05-03 16:16:27 +0800

    Added logs in admin.

--- src/server/database.js
+++ src/server/database.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host : 'localhost',
   user : 'root',
-  password : 'admin',
+  password : 'root',
   db : 'malicsi',
   dateStrings: true,
   timezone: '+08:00'
